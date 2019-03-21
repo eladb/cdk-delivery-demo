ca550d9f2abbdfa8aa426361e5fbe9f29b1310db
Elad Ben-Israel <benisrae@amazon.com>
2019-03-21 20:58:05 +0200

    deploy 5 stacks

--- bin/cdk-delivery-demo.ts
+++ bin/cdk-delivery-demo.ts
@@ -7,17 +7,21 @@ import { DeployStackAction } from '../lib/app-delivery/deploy-stack-action';
 
 const app = new cdk.App();
 
+const actions = new Array<DeployStackAction>();
+
+for (let i = 0; i < 5; ++i) {
+  actions.push(new DeployStackAction({
+    stack: new CdkDeliveryDemoStack(app, `CdkDeliveryDemoStack-${i}`),
+    admin: true
+  }));
+}
+
 new ApplicationPipelineStack(app, 'cdk-delivery-demo-application-pipeline', {
   bootstrap: 'cdk-delivery-demo',
   stages: [
     {
       name: 'Deploy',
-      actions: [
-        new DeployStackAction({
-          stack: new CdkDeliveryDemoStack(app, 'CdkDeliveryDemoStack'),
-          admin: true
-        })
-      ]
+      actions
     }
   ]
 });
